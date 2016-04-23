4e085a48f457bdeaae2f9c2a145de3c82a8d98f1
Rafael Oliveira <rafael.oliveira@liferay.com>
2016-04-23 20:19:42 -0300

    Update prod files

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -44,6 +44,13 @@ module.exports = {
         {
             test: /bootstrap-sass\/assets\/javascripts\//,
             loader: 'imports?jQuery=jquery'
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+                'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
         }
         ]
     }
